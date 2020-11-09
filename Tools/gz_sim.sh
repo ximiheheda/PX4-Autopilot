6ad55f98be90396e9c65e5512843612e79d174f6
Benjamin Perseghetti <perseghetti@gmail.com>
2020-11-09 02:32:09 -0500

    updated tools to use new jinja gen and json for gazebo sim

--- Tools/gz_sim.sh
+++ Tools/gz_sim.sh
@@ -20,82 +20,51 @@ function spawn_model() {
 	MODEL=$1
 	N=$2 #Instance Number
 	WORLD_FILE=$3
-	NTHREADS=$4
-	HITL_MODEL_NAME="temp_${MODEL}_hitl"
+	MJ=$4
 	SITL_MODEL_NAME="${MODEL}_${N}"
-
-	SUPPORTED_HITL_MODELS=("iris" "plane" "standard_vtol")
-	if [[ " ${SUPPORTED_HITL_MODELS[*]} " != *"$MODEL "* ]] && [ $hitl == true ]; then
-		echo "ERROR: Currently vehicle model $MODEL is not supported for HITL!"
-		echo "	   Supported HITL Models: [${SUPPORTED_HITL_MODELS[@]}]"
-		exit 1
-	fi
-	SUPPORTED_SITL_MODELS=("iris" "plane" "standard_vtol" "cupcar")
-	if [[ " ${SUPPORTED_SITL_MODELS[*]} " != *"$MODEL "* ]] && [ $hitl == false ]; then
-		echo "ERROR: Currently vehicle model $MODEL is not supported for SITL!"
-		echo "	   Supported SITL Models: [${SUPPORTED_SITL_MODELS[@]}]"
-		exit 1
-	fi
 	sitl_path=${SCRIPT_DIR}/sitl_gazebo
-	jinja_script=${sitl_path}/scripts/jinja_gen.py
-	jinja_model=${sitl_path}/models/${MODEL}/${MODEL}.sdf.jinja
-	
+	jinja_model_script=${sitl_path}/scripts/jinja_model_gen.py
+	base_model="--base_model ${MODEL}"
+	model_json="--json_gen ${MJ}"
+
 	if [ $hitl == true ]; then
 		python3 ${src_path}/Tools/reboot_mavlink_shell.py
-		mkdir -p ${sitl_path}/models/${HITL_MODEL_NAME}
-		serial_enabled="--serial_enabled 1"
 		hil_mode="--hil_mode 1"
-		mavlink_tcp="--mavlink_tcp_port 4560"
-		mavlink_udp="--mavlink_udp_port 14560"
-		enable_lockstep="--enable_lockstep 0"
-		output_file="--output-file ${sitl_path}/models/${HITL_MODEL_NAME}/${HITL_MODEL_NAME}.sdf"
-		jinja_world=${sitl_path}/worlds/${WORLD_FILE}.world.jinja
-		output_world="--output-file ${sitl_path}/worlds/temp_${WORLD_FILE}_hitl.world"
-		model_name="--model_name ${HITL_MODEL_NAME}"
-		ode_threads="--ode_threads ${NTHREADS}"
-		jinja_config=${sitl_path}/scripts/model.config.jinja
-		output_config="--output-file ${sitl_path}/models/${HITL_MODEL_NAME}/model.config"
-
-		python3 $jinja_script $jinja_model $sitl_path $model_name $enable_lockstep $mavlink_tcp $mavlink_udp $serial_enabled $serial_device $serial_baudrate $hil_mode $output_file
-		python3 $jinja_script $jinja_world $sitl_path $model_name $ode_threads $output_world
-		python3 $jinja_script $jinja_config $sitl_path $model_name $output_config
+		model_name="--model_name ${MODEL}"
+		python3 $jinja_model_script $base_model $model_name $hil_mode $model_json
 		sleep 1
 		source ${src_path}/Tools/setup_gazebo.bash ${src_path} ${src_path}/build/${target}
 		sleep 2
-		gazebo ${sitl_path}/worlds/temp_${WORLD_FILE}_hitl.world --verbose
+		gazebo ${sitl_path}/worlds/temp_${WORLD_FILE}.world --verbose
 
 	else
 		mavlink_tcp="--mavlink_tcp_port $((4560+${N}))"
 		mavlink_udp="--mavlink_udp_port $((14560+${N}))"
-		serial_enabled="--serial_enabled 0"
-		serial_device="--serial_device /dev/ttyACM0"
-		serial_baudrate="--serial_baudrate 921600"
-		enable_lockstep="--enable_lockstep 1"
-		hil_mode="--hil_mode 0"
 		model_name="--model_name ${SITL_MODEL_NAME}"
-		output_file="--output-file /tmp/${SITL_MODEL_NAME}.sdf"
+		output_path="--output_path /tmp"
 		working_dir="$build_path/instance_$n"
 		[ ! -d "$working_dir" ] && mkdir -p "$working_dir"
 		pushd "$working_dir" &>/dev/null
 		echo "starting instance $N in $(pwd)"
 		../bin/px4 -i $N -d "$build_path/etc" -w sitl_${SITL_MODEL_NAME} -s etc/init.d-posix/rcS >out.log 2>err.log &
-		python3 $jinja_script $jinja_model $sitl_path $enable_lockstep $model_name $mavlink_tcp $mavlink_udp $serial_enabled $serial_device $serial_baudrate $hil_mode $output_file
-		echo "Spawning ${MODEL}_${N}"
-		gz model --spawn-file=/tmp/${SITL_MODEL_NAME}.sdf --model-name=${SITL_MODEL_NAME} -x 0.0 -y $((3*${N})) -z .2	
+		python3 $jinja_model_script $base_model $model_name $mavlink_tcp $mavlink_udp $output_path $model_json
+		echo "Spawning ${SITL_MODEL_NAME}"
+		gz model --spawn-file=/tmp/${SITL_MODEL_NAME}.sdf --model-name=${SITL_MODEL_NAME} -x 0.0 -y $((3*${N})) -z 0.2	
 		popd &>/dev/null
 	fi
 	
 }
 
 if [ "$1" == "--help" ]; then
-	echo "Usage: $0 [-n <num_vehicles>] [-m <vehicle_model>] [-h <run_hitl>] [-w <world>] [-s <script>] [-t <num_threads>]"
+	echo "Usage: $0 [-n <num_vehicles>] [-m <vehicle_model>] [-h <run_hitl>] [-w <world>] [-s <script>] [-t <num_threads>] [-j <json_params>]"
 	echo "-s flag is used to script spawning vehicles e.g. $0 -s iris:3,plane:2"
 	echo "-h flag is used to launch a single vehicle in HITL mode"
 	echo "-t flag is used to set the number of ODE threads for the world"
+	echo "-j flag is used to enable json parameters from gen_params.json for the world (w), model (m), or both (wm or mw)"
 	exit 1
 fi
 
-while getopts n:m:h:w:s:t:p option
+while getopts n:m:h:w:s:t:j:p option
 do
 	case "${option}"
 	in
@@ -105,6 +74,7 @@ do
 		w) WORLD=${OPTARG};;
 		s) SCRIPT=${OPTARG};;
 		t) THREADS=${OPTARG};;
+		j) JSON=${OPTARG};;
 		p) TARGET=${OPTARG};;
 	esac
 done
@@ -113,6 +83,7 @@ num_vehicles=${NUM_VEHICLES:=3}
 world=${WORLD:=empty}
 hitl=${HITL:=false}
 threads=${THREADS:=1}
+json_opts=${JSON:=0}
 target=${TARGET:=px4_sitl_default}
 system_threads=`grep -Pc '^processor\t' /proc/cpuinfo`
 echo "Number of requested ODE threads: $((threads))"
@@ -127,6 +98,23 @@ else
 	echo "Using $threads threads for ODE."
 fi
 
+if [ "$json_opts" == "mw" ] || [ "$json_opts" == "wm" ]; then
+	echo "JSON used for both world and model generation"
+	wjson="1"
+	mjson="1"
+elif [ "$json_opts" == "w" ]; then
+	echo "JSON used for world generation"
+	wjson="1"
+	mjson="0"
+elif [ "$json_opts" == "m" ]; then
+	echo "JSON used for model generation"
+	mjson="1"
+	wjson="0"
+else 
+	wjson="0"
+	mjson="0"
+fi
+
 if [ "$hitl" == "True" ] || [ "$hitl" == "1" ] || [ "$hitl" == "true" ]; then
 	hitl=true
 else
@@ -137,37 +125,33 @@ export PX4_SIM_MODEL=${VEHICLE_MODEL:=iris}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 src_path="$SCRIPT_DIR/.."
+echo ${SCRIPT}
+build_path=${src_path}/build/${target}
+source ${src_path}/Tools/setup_gazebo.bash ${src_path} ${src_path}/build/${target}
+sleep 1
+
+sitl_path=${SCRIPT_DIR}/sitl_gazebo
+world_name="--world_name ${world}"
+jinja_world_script=${sitl_path}/scripts/jinja_world_gen.py
+sitl_ode_threads="--ode_threads ${threads}"
+world_json="--json_gen ${wjson}"
 
 if [ $hitl == true ]; then
+	hitl_model_name="--model_name ${PX4_SIM_MODEL}"
 	echo "HITL mode is currently turned on, disabling multiple vehicle spawn and script spawn."
-	spawn_model ${PX4_SIM_MODEL} 0 ${world} ${threads}
+	echo "RUNNING: python3 $jinja_world_script $world_name $hitl_model_name $world_json"
+	python3 $jinja_world_script $world_name $hitl_model_name $world_json
+	echo "Generated temp_${world}.world"
+	spawn_model ${PX4_SIM_MODEL} 0 ${world} ${mjson}
 
 else
-	echo ${SCRIPT}
-
-	build_path=${src_path}/build/${target}
-	mavlink_udp_port=14560
-	mavlink_tcp_port=4560
-
 	echo "killing running instances"
 	pkill -x px4 || true
-
-	sleep 1
-
-	source ${src_path}/Tools/setup_gazebo.bash ${src_path} ${src_path}/build/${target}
 	echo "HITL mode is currently turned off."
-	echo "Starting gazebo"
-
-
-
-
-	sitl_path=${SCRIPT_DIR}/sitl_gazebo
-	jinja_script=${sitl_path}/scripts/jinja_gen.py
-	sitl_jinja_world=${sitl_path}/worlds/${world}.world.jinja
-	sitl_output_world="--output-file ${sitl_path}/worlds/temp_${world}.world"
-	sitl_ode_threads="--ode_threads ${threads}"
-	python3 $jinja_script $sitl_jinja_world $sitl_path $sitl_ode_threads $sitl_output_world
+	echo "RUNNING: python3 $jinja_world_script $world_name $sitl_ode_threads $world_json"
+	python3 $jinja_world_script $world_name $sitl_ode_threads $world_json
 	echo "Generated temp_${world}.world"
+	echo "Starting gazebo"
 	gzserver ${sitl_path}/worlds/temp_${world}.world --verbose &
 	sleep 5
 
@@ -178,7 +162,7 @@ else
 			exit 1
 		fi
 		while [ $n -lt $num_vehicles ]; do
-			spawn_model ${PX4_SIM_MODEL} $n ${world}
+			spawn_model ${PX4_SIM_MODEL} $n ${world} ${mjson}
 			n=$(($n + 1))
 		done
 	else
@@ -195,7 +179,7 @@ else
 
 			m=0
 			while [ $m -lt ${target_number} ]; do
-				spawn_model ${target_vehicle} $n ${world}
+				spawn_model ${target_vehicle} $n ${world} ${mjson}
 				m=$(($m + 1))
 				n=$(($n + 1))
 			done
